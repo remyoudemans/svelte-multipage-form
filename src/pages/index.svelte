ce9e9fe876dfc8f50b7ae165050357ec0ada0429
Remy Oudemans <remyoudemans@gmail.com>
2021-01-29 18:11:39 +0000

    initial commit

---
+++ src/pages/index.svelte
@@ -0,0 +1,33 @@
+<script>
+	export let name = 'Joe';
+</script>
+
+<main>
+  <h1>Welcome to the form</h1>
+  <label>
+  Name
+  <input type='text' />
+  </label>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
