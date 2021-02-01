deea847d0fce1bfe848d72adc8c525d0faac26c7
remyoudemans <remyoudemans@gmail.com>
2021-02-01 17:10:33 +0000

    refactor: clean up final cat fact fetch

--- src/components/Success.svelte
+++ src/components/Success.svelte
@@ -1,25 +1,25 @@
 <script>
   import { onMount } from 'svelte';
 
-  let catFact;
-  let failed = false;
+  const getCatFact = async () => {
+    const res = await fetch('https://cat-fact.herokuapp.com/facts/random');
 
-  onMount(async () => {
-    try {
-      const res = await fetch('https://cat-fact.herokuapp.com/facts/random');
-
-      if (!res.ok) {
-        throw new Error('Failed to fetch cat fact')
-      }
-
-      const json = await res.json();
-      catFact = json.text;
-    } catch(e) {
-      failed = true;
+    if (!res.ok) {
+      throw new Error('Failed to fetch cat fact')
     }
-	});
+
+    const json = await res.json();
+    return json.text;
+	}
 </script>
 
 <h2>Thanks for your time! You are a ⭐ </h2>
 <h3>Did you know?</h3>
-<p>{failed ? 'Never mind. Nothing to see here' : catFact}</p>
+
+{#await getCatFact()}
+  <p>loading!</p>
+{:then fact}
+  <p>{fact}</p>
+{:catch error}
+  <p>No such luck. This is an error message.</p>
+{/await}
