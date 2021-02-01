ce27c3ef8567cd7c4ac640734640dd45c2fc95e6
remyoudemans <remyoudemans@gmail.com>
2021-02-01 11:59:05 +0000

    feat: add a 3rd question

---
+++ src/components/questions/Question3.svelte
@@ -0,0 +1,14 @@
+<script>
+  import formData from '../../stores/formData'
+</script>
+
+<label>
+  Favorite color
+  <input type='color' bind:value={$formData.favoriteColor} />
+</label>
+
+<style>
+  input {
+    padding: 0;
+  }
+</style>
--- src/components/questions/index.js
+++ src/components/questions/index.js
@@ -1,3 +1,4 @@
-export { default as Question0 } from './Question0.svelte'
-export { default as Question1 } from './Question1.svelte'
-export { default as Question2 } from './Question2.svelte'
+export { default as Question0 } from "./Question0.svelte";
+export { default as Question1 } from "./Question1.svelte";
+export { default as Question2 } from "./Question2.svelte";
+export { default as Question3 } from "./Question3.svelte";
